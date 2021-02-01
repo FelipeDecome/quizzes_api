d8e5e24c0359413a831497a038ae0dbe893e224d
Felipe Decome <felipe.decome@hotmail.com>
2021-02-01 16:00:17 -0300

    quiz creation route and service implemented

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import QuizzesRepository from '../repositories/typeorm/QuizzesRepository';
+import CreateQuizService from '../services/CreateQuizService';
 
 const routes = Router();
 
-routes.post('/', (request, response) => {
-  const { quiz } = request.body;
+const quizzesRepository = new QuizzesRepository();
+
+routes.post('/quiz', async (request, response) => {
+  const { title, questions } = request.body;
+
+  const createQuizService = new CreateQuizService(quizzesRepository);
+
+  const quiz = await createQuizService.execute({ title, questions });
 
   return response.status(201).json(quiz);
 });
---
+++ src/services/CreateQuizService.ts
@@ -0,0 +1,47 @@
+import Quiz from '../entities/Quiz';
+import IQuizzesRepository from '../repositories/IQuizzesRepository';
+
+interface IAnswer {
+  text: string;
+  is_correct: boolean;
+}
+
+interface IQuestion {
+  text: string;
+  answers: IAnswer[];
+}
+
+interface IRequest {
+  title: string;
+  questions: IQuestion[];
+}
+
+export default class CreateQuizService {
+  constructor(private quizzesRepository: IQuizzesRepository) {}
+
+  public async execute({ title, questions }: IRequest): Promise<Quiz> {
+    questions.forEach(question => {
+      const { answers } = question;
+
+      if (answers.length < 3 && answers.length > 4)
+        throw new Error(
+          'Questions must have at least 3 possible answers and at maximun 4.',
+        );
+
+      const correctAnswer = answers.filter(answer => answer.is_correct);
+
+      if (correctAnswer.length <= 0)
+        throw new Error('Questions must have 1 correct answer.');
+
+      if (correctAnswer.length > 1)
+        throw new Error('Questions must have only 1 correct answer.');
+    });
+
+    const quiz = await this.quizzesRepository.create({
+      title,
+      questions,
+    });
+
+    return quiz;
+  }
+}
