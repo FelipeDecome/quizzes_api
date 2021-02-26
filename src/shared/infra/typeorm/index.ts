5ff77e3129df2413b933b96806915d5233613c05
Felipe Decome <felipe.decome@hotmail.com>
2021-02-26 14:23:34 -0300

    database initial configuration

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,20 @@
-import { createConnection } from 'typeorm';
+import {
+  Connection,
+  createConnection as createTypeormConnection,
+  getConnectionOptions,
+} from 'typeorm';
 
-createConnection();
+const createConnection = async (): Promise<Connection> => {
+  const connectionOptions = await getConnectionOptions();
+
+  return createTypeormConnection(
+    Object.assign(connectionOptions, {
+      name:
+        process.env.NODE_ENV === 'test'
+          ? 'database_test.sqlite'
+          : connectionOptions.database,
+    }),
+  );
+};
+
+export default createConnection();
